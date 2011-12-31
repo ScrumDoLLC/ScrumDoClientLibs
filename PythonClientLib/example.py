a4da33da162d0ac70cf46d4dfbb0a2e5c2d75c26
Marc Hughes <mhughes@tomsnyder.com>
2011-12-31 12:55:25 -0500

    Added scrum log api calls.  Switched to using Slumber for api access

--- PythonClientLib/example.py
+++ PythonClientLib/example.py
@@ -7,26 +7,60 @@ print "ScrumDo API Example"
 
 def main():
     parser = OptionParser()
-    parser.add_option("-l", "--login", action="store_true", dest="login", default=False, help="Attempt to log in")
-    parser.add_option("-n", "--news", action="store_true", dest="news", default=False, help="Retrieve news items")
+    parser.add_option("--login", action="store_true", dest="login", default=False, help="Attempt to log in")    
+    parser.add_option("--scrumlog", action="store_true", dest="scrumlog", default=False, help="Retrieve Scrum Log items")        
+    parser.add_option("--news", action="store_true", dest="news", default=False, help="Retrieve news items")
+    parser.add_option("--project", action="store_true", dest="project", default=False, help="Retrieve news items")
+    
+    
+    parser.add_option("--post-scrumlog",        action="store_true", dest="postscrumlog", default=False, help="Post a scrum log")    
+    parser.add_option("--project-id",           action="store",      type="int", dest="project_id", default=-1, help="When posting a scrumlog, what project id to post to")    
+    parser.add_option("--message",              action="store",      type="string", dest="message", default="", help="When posting a scrumlog, what message to post")
+    parser.add_option("--message-type",         action="store",      type="int", dest="message_type", default=0, help="When posting a scrumlog, what message type to use (0=normal, 1=group note, 2=source control, 3=automated test)")    
+    parser.add_option("--category",             action="store",      type="string", dest="category", default="", help="When posting a scrumlog, what category to use")
+    parser.add_option("--related_iteration_id", action="store",      type="int", dest="related_iteration_id", default=-1, help="When posting a scrumlog, what iteration")
+    parser.add_option("--flagged",              action="store_true",      dest="flagged", default=False, help="When posting a scrumlog, should it be flagged important?")
+        
     parser.add_option("-a", "--apikey", action="store", dest="api_key", default="", help="Program API key")
     parser.add_option("-u", "--username", action="store", dest="username", default="", help="Username to log in with")
     parser.add_option("-p", "--password", action="store", dest="password", default="", help="Password to log in with")
     
+    parser.add_option( "--id", type="int", action="store", dest="id", default=-1, help="ID of object to return")
     parser.add_option( "--limit", type="int", action="store", dest="limit", default=20, help="Limit to the number of records to return")
-    parser.add_option( "--offset", type="int", action="store", dest="offset", default=20, help="Offset of records to return")
+    parser.add_option( "--offset", type="int", action="store", dest="offset", default=0, help="Offset of records to return")
     
 
     (options, args) = parser.parse_args()
 
     if options.login:
         login(options.username, options.password, options.api_key)
+    elif options.project:
+        project(options.username, options.limit, options.offset, id=options.id)
     elif options.news:
-        news(options.username, options.limit, options.offset)
+        news(options.username, options.limit, options.offset, id=options.id)
+    elif options.scrumlog:
+        scrumlog(options.username, options.limit, options.offset, id=options.id)
+    elif options.postscrumlog:
+        post_scrum_log(options.username, options.project_id, options.message, options.category, options.related_iteration_id, options.flagged, options.message_type)
     else:
         parser.print_help()
-    
-    
+
+        # creator = models.ForeignKey(User, null=True)
+        # project = models.ForeignKey("projects.Project", related_name="log_items" )
+        # date = models.DateTimeField(auto_now_add=True )
+        # message = models.TextField()
+        # 
+        # # The type of the message.
+        # #   TYPE_NOTE, TYPE_SOURCE_CONTROL    
+        # message_type = models.IntegerField()
+        # 
+        # category = models.CharField(max_length=32, default="Status Update")
+        # related_iteration = models.ForeignKey("projects.Iteration", null=True)
+        # 
+        # # The icon to use
+        # icon = models.IntegerField(default=1)        
+        # flagged = models.BooleanField(default=False)
+        
 def login(username, password, api_key):
     key = scrumdo.login(username, password, api_key)
     if key:
@@ -34,10 +68,22 @@ def login(username, password, api_key):
         f.write(key)
         f.close()
         print "Successfully logged on, auth key written to user_key file - %s " % key
-    
 
-def news(username, limit, offset):
-    result = scrumdo.news_items( username, _userKey() , limit=limit, offset=offset)
+def project(username, limit, offset, id):
+    result = scrumdo.project( username, _userKey() , limit=limit, offset=offset, id=id)    
+    print json.dumps( result , sort_keys=True, indent=4)   
+    # First epic ID: result["epics"][0]
+
+def post_scrum_log(username, project_id, message, category, related_iteration_id, flagged,message_type):
+    result = scrumdo.post_scrum_log( username, _userKey() , project_id, message, category, related_iteration_id, flagged,message_type)    
+    print result
+    
+def scrumlog(username, limit, offset, id):    
+    result = scrumdo.scrum_log( username, _userKey() , limit=limit, offset=offset, id=id)    
+    print json.dumps( result , sort_keys=True, indent=4)
+
+def news(username, limit, offset, id):
+    result = scrumdo.news_items( username, _userKey() , limit=limit, offset=offset, id=id)
     print json.dumps( result , sort_keys=True, indent=4)
 
 
--- PythonClientLib/scrumdo.py
+++ PythonClientLib/scrumdo.py
@@ -1,9 +1,20 @@
 import json
 import httplib2
+import slumber
+import re
 
 API_VERSION="v1"
 BASE_URL="http://localhost:8000/"
 DEVELOPER_API_KEY=""
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def extract_resource_id(resourceURI):
+    m = re.match("/api/v[0-9]+/[^/]+/([0-9]+)/",resourceURI);
+    return m.group(1)
 
 def login(username, password, developer_api_key=None):
     """Attempts to log in, returns an authentication key for the user if successful."""
@@ -18,15 +29,45 @@ def login(username, password, developer_api_key=None):
         return result["key"]
     return None
 
+def project( user_name, user_key, limit=20, offset=0, id=-1):
+    api = slumber.API("%sapi/%s/" % (BASE_URL,API_VERSION ) )
+    if id == -1:
+        return api.project.get(user_name=user_name,user_key=user_key,limit=limit,offset=offset)
+    else:
+        return api.project(id).get(user_name=user_name,user_key=user_key,limit=limit,offset=offset)
 
-def news_items( user_name, user_key, limit=20, offset=20, developer_api_key=None):
-    if not developer_api_key:
-        developer_api_key = DEVELOPER_API_KEY
-    url = "%sapi/%s/news?user_name=%s&user_key=%s&limit=%d&offset=%d" % (BASE_URL, API_VERSION, user_name, user_key,limit,offset)
-    print url
-    h = httplib2.Http()
-    resp, content = h.request(url)
-    if resp.status == 200:
-        result = json.loads(content)
-        return result
-    return None
+def news_items( user_name, user_key, limit=20, offset=0, id=-1):
+    api = slumber.API("%sapi/%s/" % (BASE_URL,API_VERSION ) )
+    if id == -1:
+        return api.news.get(user_name=user_name,user_key=user_key,limit=limit,offset=offset)
+    else:
+        return api.news(id).get(user_name=user_name,user_key=user_key,limit=limit,offset=offset)
+
+def post_scrum_log( user_name, user_key , project_id, message, category, related_iteration_id, flagged, message_type):
+    api = slumber.API("%sapi/%s/" % (BASE_URL,API_VERSION ))
+    try:
+        project = "/api/v1/project/%d/" % project_id
+        if related_iteration_id == -1:
+            related_iteration = None
+        else:
+            related_iteration = "/api/v1/iteration/%d/" % related_iteration_id
+        return api.scrumlog.post({"category":category,
+                                  "message":message,
+                                  "project":project,
+                                  "flagged": flagged,
+                                  "message_type": message_type,
+                                  "related_iteration":related_iteration},user_name=user_name,user_key=user_key)
+    except slumber.exceptions.HttpServerError as e:
+        print ">>>>>>>>>>>>> Returned error"
+        print e.content
+    except slumber.exceptions.HttpClientError as e:
+        print ">>>>>>>>>>>>> Returned error"
+        print e.content
+    
+def scrum_log( user_name, user_key, limit=20, offset=0, id=-1):        
+    api = slumber.API("%sapi/%s/" % (BASE_URL,API_VERSION ))
+    if id == -1:
+        return api.scrumlog.get(user_name=user_name,user_key=user_key,limit=limit,offset=offset)
+    else:
+        return api.scrumlog(id).get(user_name=user_name,user_key=user_key,limit=limit,offset=offset)
+    
